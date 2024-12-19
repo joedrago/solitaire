7a2a656f98f45e432618815dac841cc8248ab1dc
Joe Drago <joedrago@gmail.com>
2024-12-19 02:30:57 -0800

    decaf fullscreen

---
+++ src/fullscreen.js
@@ -0,0 +1,67 @@
+const fullscreen = {
+    available() {
+        const available = false
+        if (document.documentElement.requestFullScreen) {
+            return true
+        }
+        if (document.documentElement.mozRequestFullScreen) {
+            return true
+        }
+        if (document.documentElement.webkitRequestFullScreen) {
+            return true
+        }
+        return false
+    },
+
+    active() {
+        const active =
+            document.fullscreenElement ||
+            document.webkitFullscreenElement ||
+            document.mozFullScreenElement ||
+            document.msFullscreenElement
+        return active != undefined
+    },
+
+    enable(enabled) {
+        if (!fullscreen.available()) {
+            return
+        }
+        if (enabled == fullscreen.active()) {
+            return
+        }
+        if (enabled) {
+            if (document.documentElement.requestFullScreen) {
+                return document.documentElement.requestFullScreen()
+            }
+            if (document.documentElement.mozRequestFullScreen) {
+                return document.documentElement.mozRequestFullScreen()
+            }
+            if (document.documentElement.webkitRequestFullScreen) {
+                return document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT)
+            }
+        } else {
+            if (document.cancelFullScreen) {
+                return document.cancelFullScreen()
+            }
+            if (document.mozCancelFullScreen) {
+                return document.mozCancelFullScreen()
+            }
+            if (document.webkitCancelFullScreen) {
+                return document.webkitCancelFullScreen()
+            }
+        }
+    },
+
+    // alias
+    disable() {
+        return fullscreen.enable(false)
+    },
+
+    // helper
+    toggle() {
+        const active = fullscreen.active()
+        return fullscreen.enable(!active)
+    }
+}
+
+export default fullscreen
